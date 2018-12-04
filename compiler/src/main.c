42fd5597e9bf0f1d1eb8314d7a33c7f074c56389
Bagus Nurtomo <bagusthanatos@gmail.com>
2018-12-04 07:25:45 +0700

    initial work on maxargs, it's so wrong

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "util.h"
 #include "slp.h"
 
@@ -8,13 +9,21 @@ inline int maxFrom(int a,
 }
 
 int maxargs(A_stm stm) {
-    if (stm -> kind == A_assignStm) return 0;
+    if (stm -> kind == A_assignStm) {
+        if (stm -> u.assign.exp -> kind == A_eseqExp) 
+            return maxargs(stm -> u.assign.exp -> u.eseq.stm);
+        return 0;
+    }
     if (stm -> kind == A_compoundStm) 
         return maxFrom(
         maxargs(stm-> u.compound.stm1),
         maxargs(stm -> u.compound.stm2));
-    
-    return 0;
+    // stm -> kind == A_printStm
+    A_expList list = stm -> u.print.exps;
+    printf("kind: %d\n", list -> kind);
+    printf("A_pairExpList: %d\n", A_pairExpList);
+    if (list -> kind == A_pairExpList) return 2;
+    return 1;
 }
 
 int main() {
@@ -24,5 +33,6 @@ int main() {
                                                                         A_OpExp(A_NumExp(10), A_times, A_IdExp("a")))),
                                              A_PrintStm(A_LastExpList(A_IdExp("b")))));
     assert(prog);
+    printf("%d\n", maxargs(prog));
     return 0;
 }
